30f9ff7d310f17d0f84e6a52d24c908a7816422b
Camomilte <camomilte@gmail.com>
2025-03-27 14:13:58 +0100

    debugged createProduct

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -8,7 +8,24 @@ import Product from "../models/product.model.js";
 //// /
 export const createProduct = async (req, res) => {
     try {
-        const { name, description, price, category, images } = req.body || {}; 
+        const { name, binomial, description, price, category, images } = req.body || {};
+        console.log(req.body) 
+
+        // Validate required fields
+        if( !name || !binomial || !description || !price ) {
+            // Return error status and message if any of these fields are empty
+            return res.status(400).json({ message: "Name, binomial, despcription and price are required!"});
+        };
+
+        // Create a new product document and save it to the database
+        const product = await Product.create({ name, binomial, description, price, category, images });
+
+        // Return success response with status 201
+        return res.status(201).json({
+            message: "Product created successfully", // Success message
+            data: product // Created product data
+        }); 
+
     } catch (err) {
         // Log error
         console.error("Error creating product", err);
